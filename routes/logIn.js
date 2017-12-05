8476f6d1853400b80fa477b93e862f31a29dc4aa
KonstantinKurganov <kurganovk@gmail.com>
2017-12-05 16:50:02 +0300

    начинам делать авторизацию

--- routes/logIn.js
+++ routes/logIn.js
@@ -3,10 +3,16 @@
  */
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
 
 /* GET logIn page. */
 router.get('/', function(req, res, next) {
-    res.render('logIn', {title: "Security Cams App"});
+    res.render('logIn', {title: "Security Cams App" , message: {error: false}});
+});
+
+router.post('/', function(req, res, next) {
+    console.log("Username: " + req.body.username);
+    res.redirect('/index')
 });
 
 module.exports = router;
